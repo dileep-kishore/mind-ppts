6e404b4264df0fab9c0db83631981033a3f5f643
Dileep Kishore <k.dileep1994@gmail.com>
2018-04-27 08:18:07 -0400

    Updated the URL for the MIND webpage

--- src/slides/demo_website.jsx
+++ src/slides/demo_website.jsx
@@ -5,6 +5,7 @@ import {
   Slide,
   SlideSet,
   Heading,
+  Notes,
 } from 'spectacle';
 
 export default (
@@ -15,11 +16,21 @@ export default (
       </Heading>
     </Slide>
     <Slide align="flex-start flex-start" >
+      <Notes>
+        <h4> Slide Notes </h4>
+        <ol>
+          <li> Example1: Compare Oral and Stool </li>
+          <li> Example2: Compare published data with correlation </li>
+          <li> Example3: Show node query and level-up </li>
+          <li> How can we improve the UI? </li>
+          <li> What other use-cases can you imagine? </li>
+        </ol>
+      </Notes>
       <iframe
         title="MIND"
-        src="http://microbialnet.org/v0.91.html"
+        src="http://microbialnet.org/v0.92.html"
         width="1400"
-        height="850"
+        height="1000"
         style={{ marginLeft: '-30px', paddingLeft: '-30px' }}
       />
     </Slide>

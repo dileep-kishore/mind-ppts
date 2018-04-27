3f6344f0842f4c741d465c1093bc616f163c4e83
Dileep Kishore <k.dileep1994@gmail.com>
2018-04-27 06:53:29 -0400

    Updated Introduction with notes

--- src/slides/introduction.jsx
+++ src/slides/introduction.jsx
@@ -5,6 +5,7 @@ import {
   Slide,
   List,
   SlideSet,
+  Notes,
 } from 'spectacle';
 
 import { MyListItem, MyHeading } from './components/tags';
@@ -12,6 +13,13 @@ import { MyListItem, MyHeading } from './components/tags';
 export default (
   <SlideSet>
     <Slide align="center flex-start" >
+      <Notes>
+        <h4> Slide Notes </h4>
+        <ol>
+          <li> Talk about which data sources we have and what we want to incorporate</li>
+          <li> Which experimental data-sets so far? </li>
+        </ol>
+      </Notes>
       <MyHeading>
         Introduction
       </MyHeading>
@@ -20,23 +28,30 @@ export default (
           A comprehensive database of microbial interactions from different data sources
         </MyListItem>
         <MyListItem>
-          A platform for users to analyze and visualize their data
+          A platform for users to compare, analyze and visualize their data
         </MyListItem>
         <MyListItem>
-          Data population has started with 16S rDNA sequence data
+          Data population has started with associations derived from 16S data
+          and literature
         </MyListItem>
       </List>
     </Slide>
     <Slide align="center flex-start" >
+      <Notes>
+        <h4> Slide Notes </h4>
+        <ol>
+          <li> Some terminology that would be helpful in understanding my presentation </li>
+        </ol>
+      </Notes>
       <MyHeading>
         Terminology
       </MyHeading>
       <List>
         <MyListItem>
-          <b>Operational Taxonomic Units:</b> Clusters of micro-organisms grouped by DNA sequence similarity usually in the 16S region
+          <b>Operational Taxonomic Units:</b> Clusters of micro-organisms grouped by DNA sequence similarity in the 16S region
         </MyListItem>
         <MyListItem>
-          <b>Co-occurrence Networks:</b> Networks obtained from correlation matrices of 16S rDNA samples from various environments
+          <b>Co-occurrence Networks:</b> Networks obtained from the associations inferred in 16S rDNA samples from various environments
         </MyListItem>
         <MyListItem>
           <b>Exact Sequence Variants:</b> Amplicon sequence variants (ASVs) that are resolved exactly, down to the level of single-nucleotide differences over the sequenced gene region.

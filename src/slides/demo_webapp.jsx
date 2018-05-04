37d96f2bc8faa937969abaf1036d9f327fed20dc
Dileep Kishore <k.dileep1994@gmail.com>
2018-05-04 10:09:11 -0400

    Added demonstration slide

--- src/slides/demo_webapp.jsx
+++ src/slides/demo_webapp.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {
   Slide,
   List,
-  ListItem,
+  Heading,
   SlideSet,
   Notes,
 } from 'spectacle';
@@ -12,15 +12,18 @@ import { MyListItem, MyHeading } from './components/tags';
 
 export default (
   <SlideSet>
-    <Slide align="center flex-start">
-      <MyHeading>
-        Heading
-      </MyHeading>
-      <List>
-        <MyListItem> ListItem </MyListItem>
-      </List>
+    <Slide transition={['zoom']} align="center center">
+      <Heading>
+        Demonstration
+      </Heading>
     </Slide>
     <Slide align="flex-start flex-start">
+      <Notes>
+        <h4> Slide Notes </h4>
+        <ol>
+          <li></li>
+        </ol>
+      </Notes>
       <iframe
         title="webapp"
         src="http://127.0.0.1:8050/"
